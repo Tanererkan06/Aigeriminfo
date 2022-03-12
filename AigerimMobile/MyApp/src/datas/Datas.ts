5f82033ec60417541e8cc43466ffbce27624fad5
Taner E <t4n3r@msn.com>
2022-03-12 19:05:34 +0300

    dd
    dd

--- AigerimMobile/MyApp/src/datas/Datas.ts
+++ AigerimMobile/MyApp/src/datas/Datas.ts
@@ -24,6 +24,8 @@ const reviewModels = () =>
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
   });
 
+
+  
 export const doctorsList: DoctorModel[] = [
   {
     fullName: "Prof. Dr. Zafer KESKİN",
@@ -36,8 +38,8 @@ export const doctorsList: DoctorModel[] = [
     
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
-Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae.`,
-    reviews: reviewModels()
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae.`
+, reviews: reviewModels()
   },
   {
     fullName: "Dr. Müge ASLI",
