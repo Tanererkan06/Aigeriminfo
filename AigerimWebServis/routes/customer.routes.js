aec2be82fe51387c9e97921565d8cd7a4137797f
ngulzira <37243651+ngulzira@users.noreply.github.com>
2022-03-16 08:54:15 +0600

    dd
    ddd

--- AigerimWebServis/routes/customer.routes.js
+++ AigerimWebServis/routes/customer.routes.js
@@ -328,17 +328,14 @@ ng_his_vractakvim.doktor_id='DR534'  and ng_his_vractakvim.servis_id=ng_his_glzr
          
           let user = new Object();
           
-           user.vracid = elemento[1];
-           ///const buff = Buffer.from(elemento[1],'utf-8');
-          //  const base64 = buff.toString('base64');
-            //user.resim =base64; //elemento[0];
-            user.vracids = elemento[0];
-
+            user.vracid = elemento[1]; 
             user.perbilgi =elemento[2]; 
             user.imya =elemento[3]; 
             user.familiya =elemento[4]; 
             user.ocest =elemento[5]; 
-         
+           const buff = Buffer.from(elemento[0],'utf-8');
+             const base64 = buff.toString('base64');
+             user.resim =base64; //elemento[0];
            
 
           users.push(user);
