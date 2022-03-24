f4ab339a7d51035cca2834e5d4312378f2b4036d
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-03-24 03:43:56 +0300

    dd
    dddd

---
+++ AigerimMobile/MyApp/src/redux/action/changeLangAction.js
@@ -0,0 +1,14 @@
+const changelangRu = () =>({
+    type:"ru",
+});
+const changelangEn = () =>({
+    type:"en",
+});
+const changelangTR = () =>({
+    type:"tr",
+});
+const changelangKz = () =>({
+    type:"kz",
+});
+
+export {changelangRu,changelangEn,changelangTR,changelangKz};
