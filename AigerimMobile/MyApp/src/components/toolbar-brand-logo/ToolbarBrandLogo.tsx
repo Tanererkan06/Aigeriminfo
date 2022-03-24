ad5f03f4e1271c5d72803353dad0fabcc9b20f9c
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-03-24 14:33:13 +0300

    dd
    ddd

--- AigerimMobile/MyApp/src/components/toolbar-brand-logo/ToolbarBrandLogo.tsx
+++ AigerimMobile/MyApp/src/components/toolbar-brand-logo/ToolbarBrandLogo.tsx
@@ -6,15 +6,30 @@ const height = Dimensions.get('window').height;
 
 export const ToolbarBrandLogo: React.FC = () => (
   <View>
+
+    <View
+      style={{
+        flexDirection: "row",
+      }}
+    >
       <Image style={styles.image} source={require("../../../assets/logo.jpg")} />
+      <Image style={styles.mini} source={require("../../../assets/ru.png")} />
+      <Image style={styles.mini} source={require("../../../assets/kz.png")} />
+      <Image style={styles.mini} source={require("../../../assets/en.png")} />
+      <Image style={styles.mini} source={require("../../../assets/tr.png")} />
       
+ 
+ 
+    </View>
+
 
   </View>
-  
+
 );
 
 const styles = StyleSheet.create({
-  image: { width: 150, height: 250, resizeMode: "contain",borderRadius:50 },
-  bottom: { width: 150, height: 250,marginBottom:-50, resizeMode: "contain",borderRadius:50 }
+  image: { width: 250, height: 250, resizeMode: "contain", borderRadius: 50,marginLeft:-50,marginTop:18  },
+  mini: { width: 50, height: 50, resizeMode: "contain", borderRadius: 5,marginTop:100,marginLeft:2  },
+  bottom: { width: 150, height: 250, marginBottom: -50, resizeMode: "contain", borderRadius: 50 }
 
 });
