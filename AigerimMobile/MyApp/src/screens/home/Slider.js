e6a66865f630717ff74e4a07019c669be637a80b
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-03-26 00:19:28 +0300

    fff
    fffff

--- AigerimMobile/MyApp/src/screens/home/Slider.js
+++ AigerimMobile/MyApp/src/screens/home/Slider.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState,useRef } from 'react';
 
 import {
   StyleSheet,
@@ -13,73 +13,69 @@ export const SLIDER_WIDTH = Dimensions.get('window').width + 30;
 export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.8);
 
 import Carousel from 'react-native-snap-carousel';
+ 
 
-export default class Slider extends React.Component {
+const renderItem = ({item}) => {
+  return (
+    <View
+      style={{
+        borderWidth: 1,
+        padding: 20,
+        borderRadius: 20,
+        alignItems: 'center',
+        backgroundColor: 'white',
+      }}>
+      <Image source={{uri: "http://25.46.200.59:3002/"+item.image1}} style={{width: 200, height: 200}} />
+      <Text style={{marginVertical: 10, fontSize: 20, fontWeight: 'bold'}}>
+        {item.title1}
+      </Text>
+    </View>
+  );
+};
+export default Slider = () => {
+  const [index, setIndex] = useState(0);
+  const isCarousel = useRef(null);
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeIndex: 0,
-      data: [],
-      isLoading: true,
-    }
-  }
-
-  async getMovies() {
+  const getMovies = async () => {
     try {
-      const response = await fetch('http://25.46.200.59:3002/slider');
-      const json = await response.json();
-      this.setState({ data: json });
-      console.log(json)
-    } catch (error) {
-      console.log(error);
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  }
-  componentDidMount() {
-    this.getMovies();
-  }
+     const response = await fetch('http://25.46.200.59:3002/slider');
+     const json = await response.json();
+     setData(json);
+   } catch (error) {
+     console.error(error);
+   } finally {
+     setLoading(false);
+   }
+ }
 
-  _renderItem = ({ item, index }) => {
-    return (
-      <View
-        style={{
-          /*  borderWidth: 1, */
-          padding: 20,
-          borderRadius: 20,
-          alignItems: 'center',
-          justifyContent: "center",
-          backgroundColor: 'white',
-        }}>
-        <Image source={{ uri: "http://25.46.200.59:3002/" + item.image }} style={{ width: 350, height: 300 }} />
-        <Text style={{ marginVertical: 10, fontSize: 20, fontWeight: 'bold' }}>
-          {item.title}
-        </Text>
-      </View>
-    );
-  }
-
-  render() {
-    return (
+ useEffect(() => {
+   getMovies();
+ }, []);
+  return (
+    <View style={{marginVertical: 10}}>
       <Carousel
-      loop={true}
-      autoplayInterval={3000}
-      autoplay={true}
-        ref={(c) => { this._carousel = c; }}
-        data={this.state.data}
-        renderItem={this._renderItem}
+        ref={isCarousel}
+        data={data}
+        renderItem={renderItem}
         sliderWidth={SLIDER_WIDTH}
         itemWidth={ITEM_WIDTH}
+        onSnapToItem={index => setIndex(index)}
       />
-    );
-  }
-}
+   
+    </View>
+  );
+};
 
+// define your styles
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c3e50',
   },
   image: {
     flex: 1,
