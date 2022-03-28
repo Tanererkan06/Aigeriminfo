7380842d49c166916d6503c917e15effc8c713d3
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-03-28 04:03:50 +0300

    ddd
    dddd

--- AigerimMobile/MyApp/src/components/toolbar-brand-logo/ToolbarBrandLogo.tsx
+++ AigerimMobile/MyApp/src/components/toolbar-brand-logo/ToolbarBrandLogo.tsx
@@ -16,7 +16,7 @@ export const ToolbarBrandLogo: React.FC = () => (
 );
 
 const styles = StyleSheet.create({
-  image: { width: 250, height: 250, resizeMode: "contain", borderRadius: 50, marginLeft: -50,marginTop: -95   },
+  image: { width: 250, height: 250, resizeMode: "contain",   marginLeft: -50,marginTop: -110   },
   mini: { width: 50, height: 50, resizeMode: "contain", borderRadius: 5, marginTop: 100, marginLeft: 2 },
   bottom: { width: 150, height: 250, marginBottom: -50, resizeMode: "contain", borderRadius: 50 }
 
